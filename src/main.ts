97bb415dc5fd381a64183bd80c70be9205733ff4
exaby73 <nabeelparkar99@gmail.com>
2023-08-02 01:00:13 +0530

    wip

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,34 @@
+/* eslint-disable no-console */
 import * as core from '@actions/core'
-import {wait} from './wait'
+import { extractDocTags, findDocTagDiffs } from './utils'
+import github from '@actions/github'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const extensions = core.getInput('extensions').split(',')
+    const token = core.getInput('github-token')
+    const octokit = github.getOctokit(token)
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const currentRef = github.context.ref
+    const baseRef = github.context.payload.pull_request?.base.ref
 
-    core.setOutput('time', new Date().toTimeString())
+    console.log(`currentRef: ${currentRef}`)
+    console.log(`baseRef: ${baseRef}`)
+
+    // TODO head_ref and base_ref should be inputs from the action
+    // TODO build 2 arrays and then diff the two
+    const docTags1 = extractDocTags(
+      `${process.cwd()}/quickstarts/quickstart-js`,
+      extensions
+    )
+    const docTags2 = extractDocTags(
+      `${process.cwd()}/quickstarts/quickstart-js`,
+      extensions
+    )
+    console.dir(docTags1)
+    // TODO currently will be empty since same directory
+    const docTagDiffs = findDocTagDiffs(docTags1, docTags2)
+    console.dir(docTagDiffs)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
