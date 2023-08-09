f71f7fb44f0b6a1f21b326dd028ff7a69ea933b3
exaby73 <nabeelparkar99@gmail.com>
2023-08-09 15:04:34 +0530

    Implement annotation tag differ

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,106 @@
+/* eslint-disable no-console */
 import * as core from '@actions/core'
-import {wait} from './wait'
+import { DocTagDiff, extractDocTags, findDocTagDiffs } from './utils'
+import * as github from '@actions/github'
+import * as exec from '@actions/exec'
+
+function getBaseRef(): string | undefined {
+  const baseRef = core.getInput('base-ref')
+  if (!baseRef) {
+    return undefined
+  }
+  return baseRef || github.context.payload.pull_request?.base.ref
+}
+
+function assertBaseRef(baseRef?: string): asserts baseRef is string {
+  if (!baseRef) {
+    throw new Error(
+      'Action should be run in pull request context or base-ref should be provided'
+    )
+  }
+}
+
+async function checkoutRef(ref: string, cwd: string): Promise<void> {
+  await exec.exec('git', ['fetch', 'origin', ref], { cwd })
+  await exec.exec('git', ['checkout', ref], { cwd })
+  await exec.exec('git', ['pull'], { cwd })
+}
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
     core.setOutput('time', new Date().toTimeString())
+    const extensions = core.getInput('extensions').split(',')
+
+    const baseRef = getBaseRef()
+    assertBaseRef(baseRef)
+
+    const cwd = process.env['GITHUB_WORKSPACE'] || process.cwd()
+
+    const baseRefTags = extractDocTags(cwd, extensions)
+
+    const foo = await exec.getExecOutput('git', ['branch', '-v'])
+    console.log(foo.stdout)
+
+    await checkoutRef(baseRef, cwd)
+
+    const currentRefTags = extractDocTags(cwd, extensions)
+
+    const docTagDiffs = findDocTagDiffs(currentRefTags, baseRefTags)
+
+    if (!github.context.payload.pull_request) {
+      console.dir(JSON.stringify(docTagDiffs, null, 2))
+      return
+    }
+
+    annotatePR(docTagDiffs)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
 }
 
 run()
+
+function annotatePR(docTagDiffs: DocTagDiff[]): void {
+  for (const docTag of docTagDiffs) {
+    core.warning(getWarningMessage(docTag), getAnnotationProperties(docTag))
+  }
+}
+
+function getWarningMessage(docTag: DocTagDiff): string | Error {
+  const { type, changeType } = docTag
+
+  // changeType should always be code_contents for added tags
+  if (type === 'added' && changeType === 'code_contents') {
+    return `Added doc tag ${docTag.tagName}`
+  }
+
+  // changeType should always be code_contents for removed tags
+  if (type === 'removed' && changeType === 'code_contents') {
+    return `Removed doc tag ${docTag.tagName}`
+  }
+
+  if (type === 'changed') {
+    switch (changeType) {
+      case 'code_contents':
+        return `Changed code contents of doc tag ${docTag.tagName}`
+      case 'file_path':
+        return `File renamed containing doc tag ${docTag.tagName} to ${docTag.filePath}`
+      case 'line_number':
+        return `Line number changed for doc tag ${docTag.tagName} in file ${docTag.filePath}`
+    }
+  }
+
+  return new Error(`Unknown type ${type} and changeType ${changeType}`)
+}
+
+function getAnnotationProperties(
+  docTag: DocTagDiff
+): core.AnnotationProperties | undefined {
+  const file = docTag.filePath
+  return {
+    title: docTag.tagName,
+    file,
+    startLine: docTag.codeStartLine,
+    endLine: docTag.codeEndLine
+  }
+}
---
+++ src/utils.ts
@@ -0,0 +1,185 @@
+import * as fs from 'fs'
+import * as path from 'path'
+import * as glob from 'glob'
+import * as diff from 'diff'
+
+export interface DocTag {
+  filePath: string
+  codeStartLine: number
+  codeEndLine: number
+  codeContents: string
+  tagName: string
+}
+
+export interface DocTagDiff {
+  filePath: string
+  tagName: string
+  changeType: 'file_path' | 'line_number' | 'code_contents'
+  codeContentsDiff?: string
+  codeStartLine?: number
+  codeEndLine?: number
+  type: 'added' | 'removed' | 'changed'
+}
+
+function createDocTagDiff(oldTag: DocTag, newTag: DocTag): DocTagDiff[] {
+  const diffs: DocTagDiff[] = []
+  if (oldTag.filePath !== newTag.filePath) {
+    diffs.push({
+      filePath: newTag.filePath,
+      tagName: newTag.tagName,
+      codeStartLine: newTag.codeStartLine,
+      codeEndLine: newTag.codeEndLine,
+      changeType: 'file_path',
+      type: 'changed'
+    })
+  }
+  if (
+    oldTag.codeStartLine !== newTag.codeStartLine ||
+    oldTag.codeEndLine !== newTag.codeEndLine
+  ) {
+    diffs.push({
+      filePath: newTag.filePath,
+      tagName: newTag.tagName,
+      codeStartLine: newTag.codeStartLine,
+      codeEndLine: newTag.codeEndLine,
+      changeType: 'line_number',
+      type: 'changed'
+    })
+  }
+  if (oldTag.codeContents !== newTag.codeContents) {
+    diffs.push({
+      filePath: newTag.filePath,
+      tagName: newTag.tagName,
+      changeType: 'code_contents',
+      codeStartLine: newTag.codeStartLine,
+      codeEndLine: newTag.codeEndLine,
+      codeContentsDiff: diff.createTwoFilesPatch(
+        oldTag.filePath,
+        newTag.filePath,
+        oldTag.codeContents,
+        newTag.codeContents
+      ),
+      type: 'changed'
+    })
+  }
+  return diffs
+}
+
+const DOC_TAG_START_REGEX = /\[START ([^\]]+)\]/
+const DOC_TAG_END_REGEX = /\[END ([^\]]+)\]/
+
+function findFilesInDirectory(
+  directory: string,
+  extensions: string[]
+): string[] {
+  const filePatterns = extensions.map(ext => `**/*.${ext}`)
+  return glob.sync(`{${filePatterns.join(',')}}`, { cwd: directory })
+}
+
+function processFile(filePath: string): DocTag[] {
+  const fileLines = fs.readFileSync(filePath, 'utf-8').split('\n')
+  const docTags: DocTag[] = []
+  let currentTag: Partial<DocTag> = {}
+
+  // eslint-disable-next-line github/array-foreach
+  fileLines.forEach((line, index) => {
+    const startMatch = line.match(DOC_TAG_START_REGEX)
+    const endMatch = line.match(DOC_TAG_END_REGEX)
+    if (startMatch) {
+      currentTag = {
+        filePath,
+        codeStartLine: index + 1,
+        tagName: startMatch[1],
+        codeContents: ''
+      }
+    } else if (endMatch && currentTag.tagName === endMatch[1]) {
+      currentTag.codeEndLine = index + 1
+      currentTag.codeContents = currentTag.codeContents?.trim()
+      docTags.push(currentTag as DocTag)
+      currentTag = {}
+    } else if (currentTag.codeStartLine) {
+      currentTag.codeContents += `${line}\n`
+    }
+  })
+  if (currentTag.codeStartLine && !currentTag.codeEndLine) {
+    throw new Error(
+      `Missing end tag for start tag in file ${filePath} at line ${currentTag.codeStartLine}`
+    )
+  }
+  return docTags
+}
+
+export function extractDocTags(
+  directory: string,
+  extensions: string[]
+): DocTag[] {
+  const files = findFilesInDirectory(directory, extensions)
+  let docTags: DocTag[] = []
+  for (const file of files) {
+    docTags = docTags.concat(processFile(path.join(directory, file)))
+  }
+  return docTags
+}
+
+export function findDocTagDiffs(
+  oldTags: DocTag[],
+  newTags: DocTag[]
+): DocTagDiff[] {
+  const diffs: DocTagDiff[] = []
+
+  for (const oldTag of oldTags) {
+    const newTag = newTags.find(
+      tag =>
+        tag.tagName === oldTag.tagName &&
+        // Match on extname too since there could be multiple files with the same tags
+        // e.g a kotlin and java version of the same code snippet
+        tag.filePath === oldTag.filePath
+    )
+    if (!newTag) {
+      diffs.push({
+        filePath: oldTag.filePath,
+        tagName: oldTag.tagName,
+        changeType: 'code_contents',
+        type: 'removed'
+      })
+    }
+  }
+
+  for (const newTag of newTags) {
+    const oldTag = oldTags.find(
+      tag =>
+        tag.tagName === newTag.tagName &&
+        // Match on extname too since there could be multiple files with the same tags
+        // e.g a kotlin and java version of the same code snippet
+        path.extname(tag.filePath) === path.extname(newTag.filePath)
+    )
+    if (!oldTag) {
+      diffs.push({
+        filePath: newTag.filePath,
+        tagName: newTag.tagName,
+        codeStartLine: newTag.codeStartLine,
+        codeEndLine: newTag.codeEndLine,
+        changeType: 'code_contents',
+        type: 'added'
+      })
+    } else {
+      const newDiffs = createDocTagDiff(oldTag, newTag)
+      if (newDiffs.length) diffs.push(...newDiffs)
+    }
+  }
+
+  // Find removed tags
+  for (const oldTag of oldTags) {
+    const newTag = newTags.find(tag => tag.tagName === oldTag.tagName)
+    if (!newTag) {
+      diffs.push({
+        filePath: oldTag.filePath,
+        tagName: oldTag.tagName,
+        changeType: 'code_contents',
+        type: 'removed'
+      })
+    }
+  }
+
+  return diffs
+}
